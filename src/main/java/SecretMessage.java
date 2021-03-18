8f16be851117e3ce41e7d65e7c04970f83ef7253
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-18 21:36:35 +0200

    another commit

--- src/main/java/SecretMessage.java
+++ src/main/java/SecretMessage.java
@@ -2,6 +2,21 @@ import java.io.*;
 
 public class SecretMessage {
     public static void main(String[] args ) throws IOException {
+        /*By using CharacterStream, please discover the secret message from the given "secret.txt" file considering the following instructions:
+
+*Take into consideration only capital letters from the input text
+*Consider capital X as space between words
+*The secret message need to be readed from a file
+
+secret.txt
+ tHe quick brown fox jumps over the lazy dog.
+ thE quick brown fox jumps over the Lazy dog.
+the quick brown fox jumPs over the lazy dog.
+the quick brown foX juMps over the lazy dog.
+thE quick brown fox jumps over the lazy dog
+
+Output
+HELP ME*/
         FileReader inputStream=null;
         FileWriter outputStream=null;
         try{
@@ -23,26 +38,3 @@ public class SecretMessage {
         }
     }
 }
-
-/*
-By using CharacterStream, please discover the secret message from the given "secret.txt" file considering the following instructions:
-
-Take into consideration only capital letters from the input text
-Consider capital X as space between words
-The secret message need to be readed from a file
-secret.txt
-
- tHe quick brown fox jumps over the lazy dog.
-
- thE quick brown fox jumps over the Lazy dog.
-
-the quick brown fox jumPs over the lazy dog.
-
-the quick brown foX juMps over the lazy dog.
-
-thE quick brown fox jumps over the lazy dog
-
-Output
-
-HELP ME
- */
